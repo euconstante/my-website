176a5414b12325492015b5ae2b5eb64fcca9ca85
euconstante <constanteguedert@gmail.com>
2022-06-27 21:00:18 -0300

    feat: add animation to working page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Lottie from "react-lottie";
-import animationData from "../assets/animations/105936-gear-smile.json";
+import animationData from "../assets/animations/84072-loading-time-hourglass-girl.json";
 
 import "./styles.css";
 
@@ -23,13 +23,18 @@ const IndexPage = () => {
           <br />
           <span className="index-page-message_update">COOL UPDATE</span>
           <br />
-          the new layout will be released on Friday 07.01.2022! If you need to
-          talk to me please do it{" "}
-          <a href="https://www.linkedin.com/in/biancaguedert/" target="_blank">
+          the new layout will be released on{" "}
+          <span className="index-page-date">Friday 07/01/2022</span>! <br />
+          If you need to talk to me please do it{" "}
+          <a
+            className="index-page-link"
+            href="https://www.linkedin.com/in/biancaguedert/"
+            target="_blank"
+          >
             here!
           </a>
         </p>
-        <Lottie options={defaultOptions} height={400} width={400} />
+        <Lottie options={defaultOptions} height={300} width={200} />
       </section>
     </main>
   );
