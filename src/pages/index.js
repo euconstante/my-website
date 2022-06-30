a0b2f7e81ada21ef097c1bc5a8f912eb69975aea
euconstante <constanteguedert@gmail.com>
2022-06-30 00:15:44 -0300

    feat:add mobile style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import LogoIconBlack from "../assets/icons/Logo_bithrasher_1.2.svg";
 import LogoIconSimple from "../assets/icons/Logo_bithrasher_2.svg";
 import LogoIconWhite from "../assets/icons/Logo_bithrasher_2.1.svg";
 import LogoIconPurple from "../assets/icons/Logo_bithrasher_2.3.svg";
+import BulletIcon from "../assets/icons/Logo_bithrasher_3.svg";
 import AboutFigure from "../assets/images/Foto_bithrasher.png";
+import AboutColab from "../assets/images/colab.png";
 import SquidGif from "../assets/images/giphy.gif";
 import JavaScriptLogo from "../assets/icons/javascript.svg";
 import TypeScriptLogo from "../assets/icons/typescript.svg";
@@ -24,12 +26,33 @@ const hardSkills = [
   ReduxLogo,
   TailwindLogo,
 ];
+const projectsList = [
+  " Vestibulum ac nunc ex",
+  " Vestibulum ac nunc ex",
+  " Vestibulum ac nunc ex",
+  " Vestibulum ac nunc ex",
+  " Vestibulum ac nunc ex",
+];
 
 const IndexPage = () => {
   return (
     <div className="index_page">
       <header className="page_header">
         <title>Bianca Constante portifolio</title>
+        <nav className="page_header-nav">
+          <a href="#about" className="page_header-nav-links">
+            ABOUT
+          </a>
+          <a href="#projects" className="page_header-nav-links">
+            PROJECTS
+          </a>
+          <a href="#skills" className="page_header-nav-links">
+            HARD SKILLS
+          </a>
+          <a href="#contact" className="page_header-nav-links">
+            CONTACT
+          </a>
+        </nav>
         <img
           className="page_header-logo"
           src={LogoIcon}
@@ -38,7 +61,7 @@ const IndexPage = () => {
       </header>
       <main className="page_main">
         <article className="page_main-grid">
-          <section className="page_main-about-container">
+          <section id="about" className="page_main-about-container">
             <div className="page_main-about-content">
               <img
                 src={LogoIconBlack}
@@ -79,30 +102,39 @@ const IndexPage = () => {
                 enim malesuada nec. Donec vehicula consectetur lorem non mollis.
                 Ut placerat pulvinar leo.
               </p>
-              <img
-                src={SquidGif}
-                alt="Bianca Constante logo"
-                className="page_main-cool-logo"
-              />
+              <div className="page_main-cool-figures">
+                <img
+                  src={AboutColab}
+                  alt="Bianca selection"
+                  className="page_main-cool-selection"
+                />
+                <img
+                  src={SquidGif}
+                  alt="Bianca Constante logo"
+                  className="page_main-squid-gif"
+                />
+              </div>
             </div>
           </section>
-          <section className="page_main-content-projects">
+          <section id="projects" className="page_main-content-projects">
             <img
               src={LogoIconSimple}
               alt="Bianca Constante logo"
               className="page_main-cool-logo"
             />
-            <div>
-              <h2>Projects</h2>
-              <ul>
-                <li>Link 1</li>
-                <li>Link 2</li>
-                <li>Link 3</li>
-                <li>Link 4</li>
-              </ul>
-            </div>
+            <h2 className="page_main-projects-title">Projects</h2>
+            <ul className="page_main-projects-list">
+              {projectsList.map((item) => {
+                return (
+                  <li clasName="projects_list-items">
+                    <img src={BulletIcon} className="list_bullet-icon" />
+                    {item}
+                  </li>
+                );
+              })}
+            </ul>
           </section>
-          <section className="page_main-skills-container">
+          <section id="skills" className="page_main-skills-container">
             <img
               src={LogoIconPurple}
               alt="Bianca Constante logo"
@@ -120,10 +152,50 @@ const IndexPage = () => {
                 );
               })}
             </div>
+            <div className="page_main-skills-content--mobile">
+              {hardSkills.slice(0, 3).map((item) => {
+                return (
+                  <img
+                    src={item}
+                    alt="Hard skills logo"
+                    className="page_main-skills-logo"
+                  />
+                );
+              })}
+            </div>
+            <div className="page_main-skills-content--mobile">
+              {hardSkills.slice(4, 6).map((item) => {
+                return (
+                  <img
+                    src={item}
+                    alt="Hard skills logo"
+                    className="page_main-skills-logo"
+                  />
+                );
+              })}
+            </div>
           </section>
         </article>
       </main>
-      <footer className="page_footer">Footer</footer>
+      <footer id="contact" className="page_footer">
+        <p>Developed by Bianca Constante. Designed by Rodrigo Neves</p>
+        <div className="page_footer-links">
+          <a
+            href="https://www.linkedin.com/in/biancaguedert/"
+            target="_blank"
+            className="footer-contact-link"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://github.com/euconstante"
+            target="_blank"
+            className="footer-contact-link"
+          >
+            GitHub
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
